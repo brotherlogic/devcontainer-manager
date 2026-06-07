0ae5efb3987ba5b88ba7795a48d21107ba06f78d
Brotherlogic Automation <brotherlogicautomation@gmail.com>
2026-06-06 20:43:46 -0700

    Define dashboard.proto and generate Go files (closes #160)

---
+++ proto_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/brotherlogic/devcontainer-manager/proto"
+)
+
+func TestProtoTypesExist(t *testing.T) {
+	// Try to instantiate proto messages to verify they are compiled and available
+	var req proto.ListContainersRequest
+	var resp proto.ListContainersResponse
+	var container proto.Container
+	var status proto.ContainerStatus = proto.ContainerStatus_RUNNING
+
+	t.Logf("Proto types loaded successfully: req=%T, resp=%T, container=%T, status=%v", req, resp, container, status)
+}
