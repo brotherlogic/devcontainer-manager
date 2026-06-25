578e55cd547c0e0e7d97dceb4dda8e0dc8dba784
Brotherlogic Automation <brotherlogicautomation@gmail.com>
2026-06-25 06:24:36 -0700

    Remove dcm tmux naming override

--- start-tmux.sh
+++ start-tmux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Ensure the 'prod' session exists
-if ! tmux has-session -t dcm 2>/dev/null; then
+if ! tmux has-session -t devcontainer-manager 2>/dev/null; then
   # Create a new session named 'prod', detached
   cd /workspaces/devcontainer-manager
-  tmux new-session -d -s dcm
+  tmux new-session -d -s devcontainer-manager
 fi
