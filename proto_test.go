3e18b27bfaedfe0a2f15c8c19162bc57c956703f
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-06-25 08:49:21 -0700

    Implement manager.proto. Closes #206 (#213)
    * Implement manager.proto. Closes #206
    
    * Change identifier to be a oneof message
    
    ---------
    
    Co-authored-by: Brotherlogic Automation <brotherlogicautomation@gmail.com>

--- proto_test.go
+++ proto_test.go
@@ -15,3 +15,18 @@ func TestProtoTypesExist(t *testing.T) {
 
 	t.Logf("Proto types loaded successfully: req=%T, resp=%T, container=%T, status=%v", &req, &resp, &container, status)
 }
+
+func TestManagerProtoTypesExist(t *testing.T) {
+	var upReq proto.UpRequest
+	var upResp proto.UpResponse
+	var downReq proto.DownRequest
+	var downResp proto.DownResponse
+	var listReq proto.ListRequest
+	var listResp proto.ListResponse
+	var state proto.State = proto.State_DCM_RECEIVED
+	var config proto.DevcontainerConfig
+	var identifier proto.Identifier
+
+	t.Logf("Manager proto types loaded: %T %T %T %T %T %T %v %T %T", 
+		&upReq, &upResp, &downReq, &downResp, &listReq, &listResp, state, &config, &identifier)
+}
