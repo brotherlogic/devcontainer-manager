ac2dc827bb36effa1bc57f2e765e9e7f2b736a34
brotherlogic <brotherlogic@gmail.com>
2026-07-28 13:48:52 -0700

    Fix: construct default issue startup command in processManualUpRequest

--- main.go
+++ main.go
@@ -1004,6 +1004,16 @@ func processManualUpRequest(ctx context.Context, config *proto.DevcontainerConfi
 		renameDockerContainer(config.Id)
 
 		cmdToInject := cfg.startupCommand
+		if cmdToInject == "" {
+			var issueNum int32
+			if req.GetIdentifier() != nil {
+				issueNum = req.GetIdentifier().GetIssueNumber()
+			}
+			if issueNum > 0 {
+				cmdToInject = fmt.Sprintf(`agy --dangerously-skip-permissions --prompt-interactive "Take a look at the status of issue #%d - if the label matches any of the workflows in the brotherlogic/seraphine project's .agent/workflows list then you should follow that workflow. Otherwise just suggest a path forward for the issue - do not undertake any implementation work"`, issueNum)
+			}
+		}
+
 		if cmdToInject != "" {
 			wg.Add(1)
 			go func(cid string) {
